00d4f2f0822ca3877c2260986ebf7347fdc540cd
João Vitor Silva <68408347+joaohvittor@users.noreply.github.com>
2024-11-26 21:22:02 -0300

    UltimoCommit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import  CardBemVindo  from './components/cards'
-
+import CardBemVindo from './components/cards';
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <CardBemVindo/>
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <CardBemVindo />
+          <Image source={require('../../assets/images/logo.png')}  style={{ width: 200, height: 200, resizeMode: 'contain' }}/>
+        </View>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
