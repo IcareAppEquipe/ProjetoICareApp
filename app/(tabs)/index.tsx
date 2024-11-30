ac343ecb3acfeeae081498bee1390458d5e0acbe
João Vitor Silva <68408347+joaohvittor@users.noreply.github.com>
2024-11-30 16:17:31 -0300

    refeitoContainer

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,12 +8,12 @@ import CardBemVindo from './components/cards';
 export default function HomeScreen() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
+     
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <CardBemVindo />
           <Image source={require('../../assets/images/logo.png')}  style={{ width: 200, height: 200, resizeMode: 'contain' }}/>
         </View>
-      </NavigationContainer>
+      
     </SafeAreaProvider>
   );
 }
